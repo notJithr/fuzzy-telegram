94b93feb9e12ce608d6fa02ef89945d9fe5d0ad3
notJithr <iijaiden10@gmail.com>
2026-05-29 14:16:27 +0000

    Add deal dashboard summary and styling

--- vehicle-deal-portal/app/controllers/deals_controller.rb
+++ vehicle-deal-portal/app/controllers/deals_controller.rb
@@ -3,7 +3,15 @@ class DealsController < ApplicationController
 
   # GET /deals or /deals.json
   def index
-    @deals = Deal.all
+    @deals = Deal.order(deal_date: :desc)
+
+    current_month = Date.current.beginning_of_month..Date.current.end_of_month
+
+    @total_this_month = Deal.where(deal_date: current_month).count
+    @pending_deals = Deal.where(status: Deal::PENDING_STATUSES)
+    @finalised_deals = Deal.where(status: Deal::FINAL_STATUSES)
+    @fallovers = Deal.where(status: "Fallover")
+    @delayed_deals = Deal.all.select(&:delayed?)
   end
 
   # GET /deals/1 or /deals/1.json
